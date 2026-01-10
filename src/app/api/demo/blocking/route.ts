664b812806538d2842e9ca0b08c5fe6e4507c79a
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2026-01-10 11:49:32 +0530

    add error tracking by sentry

--- src/app/api/demo/blocking/route.ts
+++ src/app/api/demo/blocking/route.ts
@@ -4,7 +4,12 @@ import { anthropic } from "@ai-sdk/anthropic"
 export async function POST() {
     const response = await generateText({
         model : anthropic('claude-3-haiku-20240307'),
-        prompt: "Write a vegetarian lasagne recipe for people"
+        prompt: "Write a vegetarian lasagne recipe for people",
+        experimental_telemetry: {
+            isEnabled: true,
+            recordInputs: true,
+            recordOutputs: true
+        }
     })
 
     return Response.json({ response  })
