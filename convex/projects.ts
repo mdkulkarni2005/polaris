ece9bd53c16754ffc0e604180ea280bdfc3245a6
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2026-01-11 21:08:53 +0530

    implement authentication verification and enhance project management features

--- convex/projects.ts
+++ convex/projects.ts
@@ -1,36 +1,45 @@
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
+import { verifyAuth } from "./auth";
 
 export const create = mutation({
   args: {
     name: v.string(),
   },
   handler: async (ctx, args) => {
-    const identity = await ctx.auth.getUserIdentity();
+    const identity = await verifyAuth(ctx)
 
-    if (!identity) {
-      throw new Error("Unthorized");
-    }
-
-    await ctx.db.insert("projects", {
+    const projectId = await ctx.db.insert("projects", {
       name: args.name,
-      ownerId: identity?.subject,
-    });
+      ownerId: identity.subject,
+      updatedAt: Date.now()
+    })
+    return projectId
+  },
+});
+
+export const getPartial = query({
+  args: {
+    limit: v.number()
+  },
+  handler: async (ctx, args) => {
+    const identity = await verifyAuth(ctx)
+
+    return await ctx.db
+      .query("projects")
+      .withIndex("by_owner", (q) => q.eq("ownerId", identity.subject))
+      .take(args.limit);
   },
 });
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    const identity = await ctx.auth.getUserIdentity();
-
-    if (!identity) {
-      return [];
-    }
+    const identity = await verifyAuth(ctx)
 
     return await ctx.db
       .query("projects")
       .withIndex("by_owner", (q) => q.eq("ownerId", identity.subject))
       .collect();
   },
-});
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,12 @@
 import { useMutation, useQuery } from "convex/react"
 import { api } from "../../convex/_generated/api"
 import { Button } from "@/components/ui/button"
+import { ProjectView } from "@/features/projects/components/projects-view"
 
-const Page = () => {
-  const projects = useQuery(api.projects.get)
-  const createProject = useMutation(api.projects.create)
+const Home = () => {
   return (
-    <div className="flex flex-col gap-2">
-      <Button onClick={() => createProject({
-        name: "New Project",
-      })}>
-        Add Project
-      </Button>
-      {projects?.map((project) => (
-        <div className="border rounded p-2 flex flex-col" key={project._id}>
-          <p>{project.name}</p>
-          <p>Owner Id: {project.ownerId}</p>
-        </div>
-      ))}
-    </div>
+    <ProjectView />
   )
 }
 
-export default Page
+export default Home
